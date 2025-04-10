da4aeb0a555b8143e49287e531c08c3f8ff467d8
ANGEL DE ALBA GARCIA <angel.dealba.garcia@gmail.com>
2025-04-10 12:44:41 -0500

    April 4/10

--- api/index.js
+++ api/index.js
@@ -1,27 +1,24 @@
 // Application dependencies
 const express = require('express');
 require('dotenv').config();
-const dbConnect = require('./config/db.js');
+const dbConnect = require('../config/db.js');
 dbConnect();
 
 // Application setup
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Route definition
-// app.use(express.static('public')); // This will display /public/index.html when the server loads
-app.get("/", (request, response) => {
-    response.send("Welcome to Marc's API!");
-});
+app.use(express.static('public')); // This will display /public/index.html when the server loads
 
-const todosRouter = require("./routes/todos");
+const todosRouter = require("../routes/todos");
 app.use("/api/todos", todosRouter);
 
-const postsRouter = require("./routes/posts");
+const postsRouter = require("../routes/posts");
 app.use("/api/posts", postsRouter);
 
 app.get("/test-err", (req, res) => {
