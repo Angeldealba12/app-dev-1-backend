ecc63c5baec4bfa444dfc8443297ee2c19b5af7e
ANGEL DE ALBA GARCIA <angel.dealba.garcia@gmail.com>
2025-05-06 14:08:12 -0500

    5/06

--- config/db.js
+++ config/db.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI)
-//     .then(() => console.log('Connected!'))
-//     .catch(err => console.log(err));
-
 const connect = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI_GUIDES);
     console.log(`Connected to MongoDB connection: ${conn.connection.host}`);
